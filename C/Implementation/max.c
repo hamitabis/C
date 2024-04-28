516524ce8a89c40dcc9075b2f785662a93bb7686
hamit <30549544+hamitabis@users.noreply.github.com>
2024-04-29 00:54:41 +0300

    Create max.c

---
+++ C/Implementation/max.c
@@ -0,0 +1,7 @@
+int max(int num, int num2, int num3) {
+    return (num > num2)
+                ?
+           (num > num3 ? num : num3)
+                :
+           (num2 > num3 ? num2 : num3);
+}
