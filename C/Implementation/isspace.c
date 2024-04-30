09f92ca13d47a0e8ccf4bb4a531fcb09a232502e
hamit <30549544+hamitabis@users.noreply.github.com>
2024-04-30 18:30:27 +0300

    Create isspace.c

---
+++ C/Implementation/isspace.c
@@ -0,0 +1,13 @@
+int isspace(int ch) {
+    int result = 0;
+    if ((ch == '\t') || (ch == '\n')) {
+        result = 1;
+    } else if ((ch == '\v') || (ch == '\f')) {
+        result = 1;
+    } else if ((ch == '\r') || (ch == ' ')) {
+        result = 1;
+    } else {
+        result = 0;
+    }
+    return result;
+}
