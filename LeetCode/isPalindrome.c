b6b5ee55fbd06091a75bc1228407ed8e7d3fa7e5
hamit <30549544+hamitabis@users.noreply.github.com>
2024-06-16 18:08:43 +0300

    Update isPalindrome.c

--- LeetCode/isPalindrome.c
+++ LeetCode/isPalindrome.c
@@ -1,12 +1,10 @@
 bool isPalindrome(int x) {
-    long long int number = 0;
-    int orjNumber = x;
+    unsigned int number = 0, orjNumber = x;
 
-    if (x < 0) return false;
-    
-    while (x > 0) {
+    if (!x) return false;
+
+    while (x > 0) 
         number = (number * 10 + x % 10), x /= 10; 
-    }
-
+    
     return (number == orjNumber);
 }
