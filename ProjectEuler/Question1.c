6640a4334506ddea79bb3c68b3a74e7d41696cde
hamit <30549544+hamitabis@users.noreply.github.com>
2024-06-18 00:35:54 +0300

    Create Question1.c

---
+++ ProjectEuler/Question1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    int total = 0;
+    
+    for (int i = 0; i < 1000; i++) {
+        if (!(i % 3) || !(i % 5)) 
+            total += i;
+    }
+
+    printf("result: %d\n", total);
+}
